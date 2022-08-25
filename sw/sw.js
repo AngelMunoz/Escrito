573b49340f70bdbaef2224e0d8a60f48c6c79d0f
Angel D. Munoz <scythes.lol@gmail.com>
2022-08-25 08:41:54 -0500

    update service worker sample

--- sw/sw.js
+++ sw/sw.js
@@ -1,48 +1,50 @@
-self.
-    importScripts(
-        'https://storage.googleapis.com/workbox-cdn/releases/6.5.2/workbox-sw.js'
-    );
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/6.5.2/workbox-sw.js"
+);
 
-workbox.setConfig();
+workbox.setConfig({ debug: true });
 const { strategies, routing } = workbox;
 
 const pathEndsWith = (url, ext) => url?.pathname?.endsWith(ext);
 const hostContains = (url, ext) => url?.host?.includes(ext);
-const isGet = (request) => request.method === 'GET';
+const isGet = (request) => request.method === "GET";
 
-const isPerlaResouce = url => url?.pathname.includes("~perla~");
-const isPerlaDependency =
-    url =>
-        hostContains(url, "cdn.skypack.dev")
-        || hostContains(url, "cdn.jsdelivr.net")
-        || hostContains(url, "ga.jspm.io")
-        || hostContains(url, "unkpg.com");
-
-
-
-routing.registerRoute(({ event, request, url }) => ["/"].includes(url.pathname), new strategies.NetworkFirst({ cacheName: "html" }));
-routing.registerRoute(({ event, request, url }) => url.pathname.includes("app.webmanifest"), new strategies.NetworkFirst({ cacheName: "manifest" }));
+const isPerlaResouce = (url) => url?.pathname.includes("~perla~");
+const isPerlaDependency = (url) =>
+  hostContains(url, "cdn.skypack.dev") ||
+  hostContains(url, "cdn.jsdelivr.net") ||
+  hostContains(url, "ga.jspm.io") ||
+  hostContains(url, "unkpg.com");
 
 routing.registerRoute(
-    ({ event, request, url }) =>
-        isGet(request)
-        && request.destination === 'script'
-        && !isPerlaResouce(url)
-        && !isPerlaDependency(url),
-    new strategies.NetworkFirst({ cacheName: 'scripts' })
+  ({ event, request, url }) => ["/"].includes(url.pathname),
+  new strategies.StaleWhileRevalidate({ cacheName: "html" })
+);
+routing.registerRoute(
+  ({ event, request, url }) => url.pathname.includes("app.webmanifest"),
+  new strategies.StaleWhileRevalidate({ cacheName: "manifest" })
 );
 
 routing.registerRoute(
-    ({ event, request, url }) =>
-        isGet(request)
-        && request.destination === 'script'
-        && !isPerlaResouce(url)
-        && isPerlaDependency(url),
-    new strategies.CacheFirst({ cacheName: 'cdn-cache' })
+  ({ event, request, url }) =>
+    isGet(request) &&
+    request.destination === "script" &&
+    !isPerlaResouce(url) &&
+    !isPerlaDependency(url),
+  new strategies.StaleWhileRevalidate({ cacheName: "scripts" })
 );
 
+routing.registerRoute(
+  ({ event, request, url }) =>
+    isGet(request) &&
+    request.destination === "script" &&
+    !isPerlaResouce(url) &&
+    isPerlaDependency(url),
+  new strategies.CacheFirst({ cacheName: "cdn-cache" })
+);
 
 routing.registerRoute(
-    ({ event, request, url }) => isGet(request) && request.destination === 'style',
-    new strategies.NetworkFirst({ cacheName: 'styles' })
+  ({ event, request, url }) =>
+    isGet(request) && request.destination === "style",
+  new strategies.StaleWhileRevalidate({ cacheName: "styles" })
 );
